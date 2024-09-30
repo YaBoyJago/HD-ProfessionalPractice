dce100c304247b18b6e55049f1fd4aff3cf0d31b
YaBoyJago <s221377201@deakin.edu.au>
2024-09-30 16:43:47 +1000

    updated

--- test/test.js
+++ test/test.js
@@ -1,17 +1,22 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../app');
-const expect = chai.expect;
-
+const app = require('../app');  // Assuming your app is in app.js
+const server = app.listen(3000);
+chai.should();
 chai.use(chaiHttp);
 
 describe('App', () => {
+  after(() => {
+    // Ensure the server is closed after the tests
+    server.close();
+  });
+
   it('should return the HTML file', (done) => {
     chai.request(app)
       .get('/')
       .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res).to.be.html;
+        res.should.have.status(200);
+        res.should.be.html;
         done();
       });
   });
